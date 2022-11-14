86d01f7014bdee8277d0cd17ea622e3b4868eb7e
Amaury Quintero <amaury.quintero@ukg.com>
2022-11-14 13:12:41 -0500

    Updates

---
+++ precache-manifest.5f3ce141ac4596e87f67a7253a62f909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "676c2d41fea4a25b6997",
+    "url": "/my-react-gh-pages/static/css/main.90696aac.chunk.css"
+  },
+  {
+    "revision": "676c2d41fea4a25b6997",
+    "url": "/my-react-gh-pages/static/js/main.525da30a.chunk.js"
+  },
+  {
+    "revision": "9bb03b8e53ad1755df92",
+    "url": "/my-react-gh-pages/static/js/runtime~main.acc8aeb1.js"
+  },
+  {
+    "revision": "c58cb75a7feaf5003ac1",
+    "url": "/my-react-gh-pages/static/js/2.e6637c1e.chunk.js"
+  },
+  {
+    "revision": "cd9dcf72c3151b386634",
+    "url": "/my-react-gh-pages/static/js/3.3331c834.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-react-gh-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b8a41d5eadf0954987177d62dc1d24df",
+    "url": "/my-react-gh-pages/index.html"
+  }
+];
